f166d284c2e2265c4a0258e9602b1d05e7de9b86
Evan Fetherolf <42451093+evanfetherolf@users.noreply.github.com>
2024-07-15 17:38:19 -0700

    Persist chat_history in database

--- api/src/main.py
+++ api/src/main.py
@@ -1,8 +1,6 @@
+import os
 from fastapi import FastAPI
-from langchain_core.messages import (
-    HumanMessage,
-    AIMessage,
-)
+from langchain_mongodb.chat_message_histories import MongoDBChatMessageHistory
 from agents.chatbot_agent import chatbot_agent_executor
 from models.chatbot_query import ChatbotInput, ChatbotOutput
 from utils.async_utils import async_retry
@@ -13,7 +11,15 @@ app = FastAPI(
 )
 
 # Instantiate an empty list of messages for chat_history.
-chat_history = []
+db_user = os.getenv("MONGODB_USERNAME")
+db_pass = os.getenv("MONGODB_PASSWORD")
+db_uri = os.getenv("MONGODB_URI")
+chat_history = MongoDBChatMessageHistory(
+    session_id="default",
+    connection_string=f"mongodb://{db_user}:{db_pass}@{db_uri}",
+    database_name="chat_history",
+    collection_name="chat_histories"
+)
 
 @async_retry(max_retries=10, delay=1)
 async def invoke_agent_with_retry(input: str):
@@ -26,7 +32,7 @@ async def invoke_agent_with_retry(input: str):
     # Pass chat_history to the agent_executor here
     return await chatbot_agent_executor.ainvoke({
         "input": input,
-        "chat_history": chat_history,
+        "chat_history": chat_history.messages,
     })
 
 @app.get("/")
@@ -36,14 +42,14 @@ async def get_status():
 @app.post("/chatbot_agent")
 async def query_chatbot_agent(input: ChatbotInput) -> ChatbotOutput:
     # Save the most recent Human message to chat_history
-    chat_history.append(HumanMessage(content=input.text))
+    print("DEBUG: /chatbot-agent called")
+    chat_history.add_user_message(input.text)
     query_response = await invoke_agent_with_retry(input.text)
 
     # Save the most recent AI message to chat_history
-    chat_history.append(AIMessage(content=query_response["output"]))
+    chat_history.add_ai_message(query_response["output"])
     query_response["intermediate_steps"] = [
         str(s) for s in query_response["intermediate_steps"]
     ]
 
-    print(chat_history)
     return query_response
